35a100fdecd61519dbe1204997835f874bfd4e64
Bill <wathiede@users.noreply.github.com>
2024-04-19 16:23:45 -0700

    Remove double semicolon in SSE example

--- core/lib/src/response/stream/sse.rs
+++ core/lib/src/response/stream/sse.rs
@@ -370,7 +370,7 @@ impl Event {
 ///
 /// ```rust
 /// # use rocket::*;
-/// use rocket::response::stream::{Event, EventStream};;
+/// use rocket::response::stream::{Event, EventStream};
 /// use rocket::tokio::time::{self, Duration};
 ///
 /// #[get("/events")]
